48d16941bf7d4917f9f157b50f87c23441761a99
MikaValjakka <mika.valjakka@hotmail.com>
2023-12-09 14:41:47 +0200

    Changes to be committed:

--- README.md
+++ README.md
@@ -1,70 +1,17 @@
-# Getting Started with Create React App
+Christmas Countdown App
+This project is a Christmas Countdown app developed using React. It showcases the usage of the useState and useEffect hooks, as well as the ability to import and integrate an external component (Particles) into a React application.
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Features
+Particle Animation: The app uses the react-tsparticles library to create a visually appealing particle animation in the background.
 
-## Available Scripts
+Countdown Timer: The main functionality of the app is to display a countdown timer to Christmas Eve, updating in real-time.
 
-In the project directory, you can run:
+Technologies Used
+React: The app is built using the React library, demonstrating the use of React hooks (useState and useEffect) for state management and side effects.
 
-### `yarn start`
+react-tsparticles: External library for creating particle animations in React.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+About the Developer
+This project is part of a developer's portfolio, showcasing skills such as React development, integration of third-party libraries, and front-end design.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Feel free to explore the code and reach out for any questions or feedback!
--- src/App.js
+++ src/App.js
@@ -1,23 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 function App() {
+  const initParticles = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
   return (
     <div className="App">
+      <Particles
+        init={initParticles}
+        options={{
+          particles: {
+            // The color of the particles/snowflakes
+            color: {
+              value: "#fff",
+            },
+            // Move the snow flakes to bottom for a realistic effect, "out" in outModes is for making them reappear on top once exited at the bottom of the page, the speed should be slow for a realistic effect
+            move: {
+              direction: "bottom",
+              enable: true,
+              outModes: "out",
+              speed: { min: 0.3, max: 3 },
+            },
+            // How many particles/snowflakes will be created when starting the effect, density is a good option to enable since it calculates the particles number based on the screen size (mobile users will be happy)
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 200,
+            },
+            // The opacity of the particles/snowflakes
+            opacity: {
+              value: 0.7,
+            },
+            // The shape of the particles/snowflakes, also custom shapes can be used, this will be discussed at the end
+            shape: {
+              type: "circle",
+            },
+            // The size of the particles/snowflakes
+            size: {
+              value: 10,
+            },
+            // A nice wobble movement
+            wobble: {
+              enable: true,
+              distance: 10,
+              speed: 10,
+            },
+            // Some depth to the effect, (the layers count by default is 100, changing max here is not affecting that count)
+            // The zIndex will affect speed, size and opacity of the particles/snowflakes, the smaller the zIndex the smaller/more transparent/slower the particles/snowflakes will be
+            zIndex: {
+              value: {
+                min: 0,
+                max: 100,
+              },
+            },
+          },
+        }}
+      />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Countdown />
       </header>
+    </div>
+  );
+}
+
+function Countdown() {
+  const COUNTDOWNTIME_TARGET = new Date("2023-12-25T00:00:00");
+  // initial state set to 0,0,0,0 just to demonstrate useState initial mount
+  const [leftTime, setLeftTime] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    // Calculate time left to COUNTDOWNTIME_TARGET, as days, hours,minutes and seconds
+    // Then passing every const as property to setLeftTime state
+    // When setLeftTime state changes, the component re-renders and time is re-painted to DOM
+    const calculateTimeLeft = () => {
+      const total = COUNTDOWNTIME_TARGET - new Date();
+      const days = Math.floor(total / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((total / (1000 * 60)) % 60);
+      const seconds = Math.floor((total / 1000) % 60);
+
+      setLeftTime({ days, hours, minutes, seconds });
+    };
+
+    // every second cal calculateTimeLeft function
+    const timer = setInterval(calculateTimeLeft, 1000);
+
+    // Cleanup function to clear the interval when the component unmounts
+    return () => clearInterval(timer);
+  }, []); // Empty dependency array ensures that this effect runs once after the initial render
+
+  return (
+    <div className="countdown">
+      <div className="content">
+        <div className="box">
+          <div className="value">{leftTime.days}</div>
+          <span className="label christmasText"> days</span>
+        </div>
+        <div className="box">
+          <div className="value">{leftTime.hours}</div>
+          <span className="label christmasText"> hours</span>
+        </div>
+        <div className="box">
+          <div className="value">{leftTime.minutes}</div>
+          <span className="label christmasText"> minutes</span>
+        </div>
+        <div className="box">
+          <div className="value">{leftTime.seconds}</div>
+          <span className="label christmasText"> seconds</span>
+        </div>
+      </div>
+      <h1 className="content christmasText tillChristmas">
+        Till Christmas Eve
+      </h1>
     </div>
   );
 }
