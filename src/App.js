c7aa585e5a9089451c134875687f7eddfb251e32
Mika Valjakka <109467893+MikaValjakka@users.noreply.github.com>
2026-01-08 10:37:15 +0200

    Update App.js
    new Date to line 86 in App.js

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ function Countdown() {
   });
 
   useEffect(() => {
-    const COUNTDOWNTIME_TARGET = new Date("2025-12-25T00:00:00");
+    const COUNTDOWNTIME_TARGET = new Date("2026-12-25T00:00:00");
     const calculateTimeLeft = () => {
       const total = COUNTDOWNTIME_TARGET - new Date();
       setLeftTime({
